9345ad42a6615b35ee52b7b8a7155858e457c7e6
Dan Raskin <Danieltraskin@gmail.com>
2022-09-22 12:06:25 -0500

    checkout table template complete

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 
 function Checkout() {
 
-    const order  = useSelector(store=>store.orderDetails)
+    const order  = useSelector(store=>store.orderDetails) //takes in fully formed data object.
 
     const postOrder = (event) => {
         event.preventDefault();
@@ -17,11 +17,36 @@ function Checkout() {
             console.log('error in postOrder: ',error);
         })
     }
-
+//to do: utilize MUI 'spanning table'
     return (
-        <form onSubmit={(event) => postOrder(event)}>
-            <button type='submit'>Submit</button>
-        </form>
+        <>
+            <h2>Step 3: Checkout</h2>
+            <p>{order.customer_name}</p>
+            <p>{order.street_address}</p>
+            <p>{order.city}, {order.zip}</p>
+            <table id="orderReview">
+                <thead>
+                    <tr>
+                        <td>Pizza</td>
+                        <td>Cost</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {order.pizzas.map(pizza=> {
+                        return (
+                            <tr>
+                                <td>pizzaid: {pizza.id}</td>
+                                <td>pizzaquant: {pizza.quantity}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            <h3>Total price: </h3>
+            <form onSubmit={(event) => postOrder(event)}>
+                <button type='submit'>Submit</button>
+            </form>
+        </>
     );
 }
 
