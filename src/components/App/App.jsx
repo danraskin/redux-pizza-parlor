b521b644f527d57c1057a4a6864c4fa443def676
Blake Smith <blake.smith.mn@gmail.com>
2022-09-22 11:16:06 -0500

    Redux Setup / Router Setup / Component Starting Points

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+// COMPONENT IMPORTS
+import Header from '../Header/Header';
+// import Checkout from '../Checkout/Checkout';
+// import Menu from '../Menu/Menu';
+// import OrderForm from '../OrderForm/OrderForm';
+// import PizzaItem from '../PizzaItem/PizzaItem';
+
+
 
 function App() {
 
+  // GET PIZZAS via AXIOS
+
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+      <Router>
+        <Header />
+        <Route exact path="/">
+          // Menu and Pizza Items
+          <img src='images/pizza_photo.png' />
+          <p>Pizza is great.</p>
+        </Route>
+        <Route exact path="/order">
+          // order form will live here
+        </Route>
+        <Route exact path="/checkout">
+          // checkout componenets
+        </Route>
+        <Route exact path="/admin">
+          // admin page components
+        </Route>
+
+
+      </Router>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// PIZZA REDUCER
+const cart = (state = [], action) => {
+    // dealing with number and id's of pizzas
+    // action type to add 
+    // action type to remove
+
+
+    return state;
+}
+
+// ORDER DETAILS
+const orderDetails = (state = [], action) => {
+    // action type to add customer info
+
+
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        cart,
+        orderDetails
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
