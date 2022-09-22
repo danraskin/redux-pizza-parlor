9bd291e1e6b96ce1b71cb7cf29801315b26493b9
Blake Smith <blake.smith.mn@gmail.com>
2022-09-22 11:30:36 -0500

    GET route should be set up with pizzas reducer returning the current state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 // COMPONENT IMPORTS
 import Header from '../Header/Header';
@@ -13,27 +15,44 @@ import Header from '../Header/Header';
 
 
 function App() {
-
+  const dispatch = useDispatch();
+  //PAGE LOAD
+  useEffect(() => {
+    fetchMenu();
+  }, []);
   // GET PIZZAS via AXIOS
-
+  const fetchMenu = () => {
+    axios({
+      method: 'GET',
+      url: '/api/pizza'
+    })
+      .then(response => {
+        console.log(response);
+        const action = {
+          type: 'GET_PIZZAS',
+          payload: response.data
+        }
+        dispatch(action);
+      })
+  }
 
   return (
     <div className='App'>
       <Router>
         <Header />
         <Route exact path="/">
-          // Menu and Pizza Items
+          {/* // Menu and Pizza Items */}
           <img src='images/pizza_photo.png' />
           <p>Pizza is great.</p>
         </Route>
         <Route exact path="/order">
-          // order form will live here
+          {/* // order form will live here */}
         </Route>
         <Route exact path="/checkout">
-          // checkout componenets
+          {/* // checkout componenets */}
         </Route>
         <Route exact path="/admin">
-          // admin page components
+          {/* // admin page components */}
         </Route>
 
 
