94f943027606fea5e45b91d98a705787abff1046
Ryan Hess <ryanmhess@gmail.com>
2022-09-23 11:18:50 -0500

    fixed stuff"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,24 +11,16 @@ function Header(totalCost) {
     const total = totalCost.totalCost;
     console.log('header', total);
 
-
-    const totalCost  = useSelector(store=>store.totalCost)
-    return (
-        <header className='App-header'>
-            <h1 className='App-title'>Prime Pizza</h1>
-            <p>Total: ${totalCost}</p>
-    return (
-        <header>
-            <AppBar position="fixed" color='secondary'>
-                <Toolbar>
-                    <Typography variant='h2' sx={{ flexGrow: 1 }}>Prime Pizza</Typography>
-
-                    <Typography variant='h5' id="ordertotal" edge="end">Total: $ {Math.max(total.toFixed(2), 0.00)}</Typography>
-
-                </Toolbar>
-            </AppBar>
-        </header>
-    )
-};
+        return (
+            <header>
+                <AppBar position="fixed" color='secondary'>
+                    <Toolbar>
+                        <Typography variant='h2' sx={{ flexGrow: 1 }}>Prime Pizza</Typography>
+                        <Typography variant='h5' id="ordertotal" edge="end">Total: $ {Math.max(total.toFixed(2), 0.00)}</Typography>
+                    </Toolbar>
+                </AppBar>
+            </header>
+        )
+    };
 
 export default Header;
